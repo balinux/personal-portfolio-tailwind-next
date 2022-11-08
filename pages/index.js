5d53623b25346b9ada91bfee7e6346f360c556f2
balinux <ryojuniyantara@gmail.com>
2022-11-08 22:19:18 +0800

    add: about me

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className=" bg-white md:mx-28">
+      <main >
         {/* navigasi */}
-        <nav className="flex flex-row justify-between items-center h-16">
+        <nav className="flex flex-row justify-between items-center h-16 bg-[#FBFBFB] md:px-28">
           {/* logo */}
           <div>
             <p className="text-4xl text-primary font-bold">R.</p>
@@ -29,29 +29,60 @@ export default function Home() {
         </nav>
 
         {/* CTA */}
-        <section className="mt-5">
-          <div className="flex flex-row">
-            <div className=" h-[70vh] w-screen overflow-hidden flex flex-col justify-center items-start">
-              <div className="full">
-                <h1 className=" font-sans text-5xl font-bold mb-4">
-                  Rio Juniyantara Putra
-                </h1>
-                <h1 className=" font-sans mb-9">
-                  Web Developer, Mobile Developer
-                </h1>
-                <button className="bg-primary text-white p-3 rounded-md font-semibold">
-                  Contact Me
-                </button>
-              </div>
+        <section className="pt-5 bg-[#FBFBFB] md:px-28">
+          <div className="flex flex-col md:flex-row">
+
+            {/* CTA */}
+            <div className=" order-2 md:order-1 md:h-[70vh] w-screen overflow-hidden flex flex-col justify-center items-start mt-5 md:mt-0">
+              <h1 className=" font-sans md:text-5xl text-3xl font-bold md:mb-4 mb-1 self-center md:self-start">
+                Rio Juniyantara Putra
+              </h1>
+              <h1 className=" font-sans mb-9 text-center md:text-left self-center md:self-start">
+                Web Developer, Mobile Developer
+              </h1>
+              <button className="bg-primary self-center md:self-start text-white p-3 rounded-md font-semibold mb-10 md:mb-0">
+                Contact Me
+              </button>
             </div>
-            <div className=" h-[70vh] w-screen flex justify-center items-center">
-              <figure className=" h-full w-3/6 bg-slate-600 overflow-hidden rounded-lg">
-                <img className=" object-cover h-full" src="/logan.jpg" alt="" />
+
+            {/* image */}
+            <div className="order-1 md:order-2 md:h-[70vh] w-full flex justify-center items-center">
+              <figure className=" flex h-1/2 w-full overflow-hidden rounded-lg justify-center">
+                <Image src="/rio.jpg" width={500} height={500} className="object-cover" />
+                {/* <img className=" object-cover h-full" src="/logan.jpg" alt="" /> */}
                 {/* <Image src="/logan.jpg" width="100%" height="100%" className="object-cover h-full" /> */}
               </figure>
             </div>
           </div>
         </section>
+
+        {/* about me */}
+        <section className="md:px-28 flex flex-col md:flex-row md:h-[50vh]">
+          <div className="  md:w-1/2 w-full flex flex-col justify-center items-center">
+            <div className="">
+              <h1 className=" text-4xl font-bold mb-2 mt-10 md:mt-0">About Me</h1>
+              <div className=" w-2/3 h-1 bg-primary"></div>
+            </div>
+          </div>
+
+          <div className="  md:w-1/2 w-full flex flex-col justify-center items-end px-8">
+            <p className=" mb-5 mt-5 text-justify">consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue</p>
+            <p className="text-justify">consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue nulla facilisi nullam vehicula ipsum a arcu cursus vitae congue</p>
+
+            <div className="flex flex-row w-full md:mt-10 mt-5">
+              <button className="bg-primary self-center md:self-start border-solid border-2 border-primary text-white p-2 rounded-md font-semibold mr-2">
+                Hobby
+              </button>
+
+              <button className=" border-solid border-2 border-primary text-primary self-center md:self-start  p-2 rounded-md font-semibold">
+                Education
+              </button>
+            </div>
+          </div>
+
+        </section>
+
+
       </main>
 
       {/* <footer className={styles.footer}>
